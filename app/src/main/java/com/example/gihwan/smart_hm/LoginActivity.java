db8d9ae48a714658eb7760eb469ab2f1fd0d5864
GiHwanHong <tdr1008@naver.com>
2018-01-15 17:44:03 +0900

    modify member join, mypage xml

--- app/src/main/java/com/example/gihwan/smart_hm/LoginActivity.java
+++ app/src/main/java/com/example/gihwan/smart_hm/LoginActivity.java
@@ -71,13 +71,13 @@ public class LoginActivity extends ActivityGroup {
 
         tabhost1.addTab(tabhost1.newTabSpec("tab1tag")                      // newTabSpec - TabSpec 객체는 탭버튼의 형태와 탭의 내용을 관리하는 객체입니다.
                 .setIndicator(getString(R.string.tab1))                     // TabSpec 객체를 생성할때 사용할 tag 이름을 지정합니다.
-                .setContent(new Intent(this, StateActivity.class)));
+                .setContent(new Intent(this, StateActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
         tabhost1.addTab(tabhost1.newTabSpec("tab2tag")
                 .setIndicator(getString(R.string.tab2))
-                .setContent(new Intent(this, ChartActivity.class)));
+                .setContent(new Intent(this, ChartActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
         tabhost1.addTab(tabhost1.newTabSpec("tab3tag")
                 .setIndicator(getString(R.string.tab3))
-                .setContent(new Intent(this,MypageActivity.class).putExtra("usr_id_recv",usr_id_recv).putExtra("usr_code_recv",usr_code_recv)));
+                .setContent(new Intent(this,MypageActivity.class).putExtra("usr_id_recv",usr_id_recv).putExtra("usr_code_recv",usr_code_recv).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
 
         createTab();
 
--- app/src/main/java/com/example/gihwan/smart_hm/StateActivity.java
+++ app/src/main/java/com/example/gihwan/smart_hm/StateActivity.java
@@ -1,13 +1,10 @@
 package com.example.gihwan.smart_hm;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -17,54 +14,51 @@ import android.widget.ToggleButton;
 
 public class StateActivity extends AppCompatActivity {
 
+    TextView TMP , Humidity;
+    TextView State_Gas , State_Fire;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
 
+        TMP = (TextView)findViewById(R.id.Tmp_r1);           // 온도 상태를 숫자로 나타내기 위한 Textview
+        Humidity = (TextView)findViewById(R.id.Tmp_r2);      // 습도 상태를 숫자로 나타내기 위한 Textview
+
+        State_Gas = (TextView)findViewById(R.id.txt_gas_state); // 가스 상태를 나타내는 Textview
+        State_Fire = (TextView)findViewById(R.id.txt_fire_state); // 화재 상태를 나타내는 Textview
 
     }
 
     public void State_ToggleBtn_click(View v){
         switch (v.getId()){
-            case R.id.switch_L_r1:      // LED 토글버튼 클릭시
-                ToggleButton tb1 = (ToggleButton) findViewById(R.id.switch_L_r1);
+            case R.id.switch_L_1:      // LED 토글버튼 클릭시
+                ToggleButton tb1 = (ToggleButton) findViewById(R.id.switch_L_1);
                 if(tb1.isChecked()){
                     Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다1 ", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.switch_L_r2:      // LED 토글버튼 클릭시
-                ToggleButton tb2 = (ToggleButton) findViewById(R.id.switch_L_r2);
+
+            case R.id.switch_L_2:      // LED 토글버튼 클릭시
+                ToggleButton tb2 = (ToggleButton) findViewById(R.id.switch_L_2);
                 if(tb2.isChecked()){
                     Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다2 ", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.switch_L_r3:      // LED 토글버튼 클릭시
-                ToggleButton tb3 = (ToggleButton) findViewById(R.id.switch_L_r3);
+
+            case R.id.switch_L_3:      // LED 토글버튼 클릭시
+                ToggleButton tb3 = (ToggleButton) findViewById(R.id.switch_L_3);
                 if(tb3.isChecked()){
                     Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다3 ", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.switch_L_r4:      // LED 토글버튼 클릭시
-                ToggleButton tb4 = (ToggleButton) findViewById(R.id.switch_L_r4);
-                if(tb4.isChecked()){
-                    Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다4 ", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.switch_G_r1:      // 가스 토글 버튼 클릭시
-                ToggleButton tb5 = (ToggleButton) findViewById(R.id.switch_G_r1);
+
+            case R.id.switch_G_1:      // 가스 토글 버튼 클릭시
+                ToggleButton tb5 = (ToggleButton) findViewById(R.id.switch_G_1);
                 if(tb5.isChecked()){
                     Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다 g1 ", Toast.LENGTH_SHORT).show();
                 }
                 break;
-
-            case R.id.switch_G_r2:      // 가스 토글 버튼 클릭시
-                ToggleButton tb6 = (ToggleButton) findViewById(R.id.switch_G_r2);
-                if(tb6.isChecked()){
-                    Toast.makeText(getApplicationContext(), "LED 토글버튼이 눌렸습니다 g2 ", Toast.LENGTH_SHORT).show();
-                }
-                break;
-
 
         }
     }
